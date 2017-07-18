73ec2d0710d88e9b0bf939029076c68d2cc0c61c
ggarcera <ggarcera@hotmail.com>
2017-07-18 15:38:52 -0600

    Build files and gulpfile.js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
-var gulp = require('gulp');
+// ////////////////////////////////////////////////
+// Required tasks
+// // /////////////////////////////////////////////
 
-gulp.task('default', function(){
-  console.log("I'm Running Gulp!");
-})
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename');
+
+// ////////////////////////////////////////////////
+// Scripts
+// use globes to minify and match the files onto the target folder
+// // /////////////////////////////////////////////
+
+gulp.task('scripts', function(){
+    gulp.src(['src/views/js/**/*.js', '!src/views/js/**/*.min.js'])
+    .pipe(rename({suffix:'.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/views/js'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/views/css/**/*.css', '!src/views/css/**/*.min.css'])
+  .pipe(rename({suffix:'.min'}))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('dist/views/css'));
+});
+
+// ////////////////////////////////////////////////
+// Watch
+// // /////////////////////////////////////////////
+
+gulp.task('watch', function(){
+  gulp.watch('src/views/js/**/*.js', ['scripts']);
+  gulp.watch('src/views/css/**/*.css', ['styles']);
+
+});
+
+// ////////////////////////////////////////////////
+// Deafult Task
+// // /////////////////////////////////////////////
+
+gulp.task('default', ['scripts', 'styles', 'watch']);
---
+++ src/views/js/test.js
@@ -0,0 +1,3 @@
+console.log("testing several files for minifyng");
+console.log("happy, happy, happy");
+console.log("happy, happy, happy");
